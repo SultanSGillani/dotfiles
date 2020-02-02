1a8009dc725ef78b2deb6257e13f742cd66356b0
Sultan Gillani <sultan@veracity.io>
2020-02-02 15:49:48 -0500

    uber

---
+++ roles/ubersicht/files/pecan/background.jsx
@@ -0,0 +1,8 @@
+const refreshFrequency = false; // ms
+
+const render = () => <div class='screen'>
+    <link rel="stylesheet" type="text/css" href="/pecan/style.css"/>
+    <div class='pecanbackground' /
+    ></div>;
+
+export {refreshFrequency, render};
